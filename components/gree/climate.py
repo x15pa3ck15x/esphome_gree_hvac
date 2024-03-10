83d3c0367289d3dd3b3f003778a0d90efd9255d0
Patress <42972161+x15pa3ck15x@users.noreply.github.com>
2024-03-10 11:04:57 +0100

    Update climate.py

--- components/gree/climate.py
+++ components/gree/climate.py
@@ -21,17 +21,17 @@ GreeClimate = gree_ns.class_(
     "GreeClimate", climate.Climate, cg.PollingComponent, uart.UARTDevice
 )
 
-# ALLOWED_CLIMATE_SWING_MODES = {
-#     "BOTH": ClimateSwingMode.CLIMATE_SWING_BOTH,
-#     "VERTICAL": ClimateSwingMode.CLIMATE_SWING_VERTICAL,
-#     "HORIZONTAL": ClimateSwingMode.CLIMATE_SWING_HORIZONTAL,
-# }
-# validate_swing_modes = cv.enum(ALLOWED_CLIMATE_SWING_MODES, upper=True)
+ALLOWED_CLIMATE_SWING_MODES = {
+    "BOTH": ClimateSwingMode.CLIMATE_SWING_BOTH,
+    "VERTICAL": ClimateSwingMode.CLIMATE_SWING_VERTICAL,
+    "HORIZONTAL": ClimateSwingMode.CLIMATE_SWING_HORIZONTAL,
+}
+validate_swing_modes = cv.enum(ALLOWED_CLIMATE_SWING_MODES, upper=True)
 
 ALLOWED_CLIMATE_PRESETS = {
     "NONE": ClimatePreset.CLIMATE_PRESET_NONE,
     "BOOST": ClimatePreset.CLIMATE_PRESET_BOOST,
-    # "SLEEP": ClimatePreset.CLIMATE_PRESET_SLEEP,
+    "SLEEP": ClimatePreset.CLIMATE_PRESET_SLEEP,
 }
 validate_presets = cv.enum(ALLOWED_CLIMATE_PRESETS, upper=True)
 
@@ -40,9 +40,9 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(GreeClimate),
             cv.Optional(CONF_SUPPORTED_PRESETS): cv.ensure_list(validate_presets),
-            # cv.Optional(CONF_SUPPORTED_SWING_MODES): cv.ensure_list(
-                # validate_swing_modes
-            # ),
+            cv.Optional(CONF_SUPPORTED_SWING_MODES): cv.ensure_list(
+                validate_swing_modes
+            ),
         }
     )
     # wifi module polls every 300ms but do we need it so often? set it to 10s
@@ -55,7 +55,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
     await uart.register_uart_device(var, config)
-    # if CONF_SUPPORTED_SWING_MODES in config:
-    # cg.add(var.set_supported_swing_modes(config[CONF_SUPPORTED_SWING_MODES]))
+    if CONF_SUPPORTED_SWING_MODES in config:
+    cg.add(var.set_supported_swing_modes(config[CONF_SUPPORTED_SWING_MODES]))
     if CONF_SUPPORTED_PRESETS in config:
         cg.add(var.set_supported_presets(config[CONF_SUPPORTED_PRESETS]))
